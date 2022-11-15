b58c93d3e5dd1fb6f396f018785d8f2de721d1cb
JuanFlorez1326 <juanks339@gmail.com>
2022-11-15 15:59:35 -0500

    Server Configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+import http from "http";
+import cors from "cors";
 import express from "express";
+import morgan from "morgan";
+import { Server } from "socket.io";
+import { PORT } from "./config.js";
+
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get("/", (req, res) => {
-        res.send("Hello World");
-    }
-);
+app.use(morgan("dev"));
+app.use(cors());
 
-app.listen(3000, () => {
-        console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+        console.log("Server is running on port " + PORT);
     }
 );
